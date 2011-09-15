823da8ae391324633d491bb8bc3c19f127014b79
JTWebMan <jtwebman@gmail.com>
2011-09-15 03:16:45 -0600

    Added full express jade stack and added some more help content

--- googleplusrss.js
+++ googleplusrss.js
@@ -4,38 +4,46 @@ var https = require('https');
 var RSS = require('rss');
 var cache = require('./cache');
 var url = require('url');
+var express = require('express');
 
-var reId = new RegExp(/\/(\d+)$/);
+//var reId = new RegExp(/\/(\d+)$/);
 
-PostTypeEnum = {
-      Plain : 0,
-      Link : 1,
-      YouTube : 2,
-      Shared : 3
-    }
 
-http.createServer(function(req, serverResponse) {
-  try {
-    var m = reId.exec(req.url); //Get the ID off the url 
-    if (m != null) { //Url has the id so send back rss feed
-      var googleId = m[1];
-      var cacheFeed = cache.get(googleId);
-      if (cacheFeed != null) { //cached version found so just return that
-        serverResponse.end(cacheFeed);
-        console.log("Read from cache: " + googleId);
-      } else { //No cached version so make a request to google.
-        var googleReq = https.request(getGoogleOptions(googleId), function(googleResponse) {
-          handleGooglesResponse(serverResponse, googleResponse, googleId);
-        });
-        googleReq.end();
-      }
-    } else { //Else output directions
-      sendRootPage(serverResponse);
-    }
-  } catch (ex) {
-    sendErrorPage(serverResponse, ex.toString());
+
+var app = require('express').createServer();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.configure(function(){
+  app.use(express.logger());
+  app.use(express.static(__dirname + '/static'));
+  //app.use(express.errorHandler({ dumbExceptions: true, showStack: true }));
+});
+
+
+app.get(/\/(\d+)$/, function(req, res){
+  var googleId = req.params[0];
+  var cacheFeed = cache.get(googleId);
+  if (cacheFeed != null) { //cached version found so just return that
+    res.end(cacheFeed);
+  } else { //No cached version so make a request to google.
+    var googleReq = https.request(getGoogleOptions(googleId), function(googleResponse) {
+      handleGooglesResponse(res, googleResponse, googleId);
+    });
+    googleReq.end();
   }
-}).listen(11908);
+});
+
+app.get('*', function(req, res){
+  res.render('index');
+});
+
+app.error(function(ex, req, res, next){
+  displayErrorPage(ex, res);
+});
+
+app.listen(11908);
 
 function getGoogleOptions(googleId) {
   return {
@@ -46,24 +54,26 @@ function getGoogleOptions(googleId) {
           headers: {
             'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110613 Firefox/6.0a2',
             'Connection':'keep-alive'
-         }
-};
+          }
+        };
 }
 
 function handleGooglesResponse(serverResponse, googleResponse, googleId) {
-  var data = [];
-  googleResponse.on('data', function(d) {
-    data.push(d);
-  });
-  googleResponse.on('end', function() {
-    console.log();
-    if (googleResponse.statusCode != 200) {
-      //output directions status code other then 200 came back from google
-      sendErrorPage(serverResponse, "Google sent back a status code of: " + googleResponse.statusCode); 
-    } else {
-      sendRSSfeed(serverResponse, data.join(''), googleId)
-    }
-  });	
+  try {
+    var data = [];
+    googleResponse.on('data', function(d) {
+      data.push(d);
+    });
+    googleResponse.on('end', function() {
+      if (googleResponse.statusCode != 200) { //output directions status code other then 200 came back from google
+        displayErrorPage("Google sent back a status code of: " + googleResponse.statusCode, serverResponse);
+      } else {
+        sendRSSfeed(serverResponse, data.join(''), googleId)
+      }
+    });	
+  } catch (ex) {
+    displayErrorPage(ex, serverResponse);
+  }
 }
 
 // To clean up the results to parse the JSON object from google
@@ -74,6 +84,7 @@ function googleJSONFilter(googleResults) {
   return googleResults.replace(/,,/g, ',"",');
 }
 
+// This is the main function to turn googles JSON into a rss feed
 function sendRSSfeed(serverResponse, googleResults, googleId) {
   try {
     var p = JSON.parse(googleJSONFilter(googleResults));
@@ -209,36 +220,19 @@ function sendRSSfeed(serverResponse, googleResults, googleId) {
 
     serverResponse.writeHead(200, { 'content-type': 'application/rss+xml' });
     serverResponse.end(feed);
-    console.log("Pulled from google: " + googleId);
   } catch (ex) {
-    sendErrorPage(serverResponse, ex.toString());
+    displayErrorPage(ex.toString(), serverResponse);
   }
 }
 
-function sendErrorPage(serverResponse, errorMsg) {
-  serverResponse.writeHead(400, { 'content-type': 'text/html' });
-  serverResponse.write("<html><head><title>Google+ to RSS Feed</title></head><body>");
-  serverResponse.write("<h1>Google+ to RSS Feed Error</h1>");
-  serverResponse.write("<p>We got the following error: " + errorMsg + "</p>");
-  serverResponse.write("<p>Please make sure you grabed the whole user id. See <a href='/'>Google+ To Rss Home Page</a>.</p>");
-  serverResponse.write("<p>This still is a beta site so it might go down from time to time. If you have any issues please submit a issue to <a href='https://github.com/jtwebman/GooglePlusToRSSFeed/issues'>Github Issues</a></p>");
-  serverResponse.end("</body></html>");
+function displayErrorPage(msg, res) {
+  res.render('404', { errorMsg: msg});
 }
 
-function sendRootPage(serverResponse) {
-  serverResponse.writeHead(200, { 'content-type': 'text/html' });
-  serverResponse.write("<html><head><title>Google+ to RSS Feed</title></head><body>");
-  serverResponse.write("<h1>Google+ to RSS Feed</h1>");
-  serverResponse.write("<p>Just add your google user id to this url like this <a href='http://googleplusrss.nodester.com/118004117219257714873'>http://googleplusrss.nodester.com/118004117219257714873</a> and it will give you your raw rss feed. You can get your user id by logging into your Google+ account and clicking on your name. The url in the browse will have a long number which is your user id.</p>");
-  serverResponse.write("<p>This still is a beta site so it might go down from time to time. If you have any issues please submit a issue to <a href='https://github.com/jtwebman/GooglePlusToRSSFeed/issues'>Github Issues</a></p>");
-  serverResponse.write("<p><b>Current Working On:</b></p>");
-  serverResponse.write("<ol>");
-  serverResponse.write("<li><del>Fixing links and videos with no text with them.</del></li>");
-  serverResponse.write("<li><del>Make all posts look close to how google+ displays them.</del></li>");
-  serverResponse.write("<li>Adding a full express site with more detailed help.</li>");
-  serverResponse.write("<li>Limits so we don't get kicked from nodester.com, it's free thanks guys!</li>");
-  serverResponse.write("<li>A job doing Node.js so I can stop wasting my time with M$ programming. :)</li>");
-  serverResponse.write("</ol>");
-  serverResponse.write("<p>Thanks for taking a look! JTWebMan</p>");
-  serverResponse.end("</body></html>");
-}
+PostTypeEnum = {
+        Plain : 0,
+        Link : 1,
+        YouTube : 2,
+        Shared : 3
+      }
+
