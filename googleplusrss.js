46814afd459108a8015d6283bcc49f9734902966
JTWebMan <jtwebman@gmail.com>
2011-11-09 17:13:47 -0700

    Basic Google API

--- googleplusrss.js
+++ googleplusrss.js
@@ -28,8 +28,8 @@ app.get(/\/(\d+)$/, function(req, res){
       res.writeHead(200, { 'content-type': 'application/rss+xml' });
       res.end(cacheFeed);
     } else { //No cached version so make a request to google.
-      var googleReq = https.request(getGoogleOptions(googleId), function(googleResponse) {
-        handleGooglesResponse(res, googleResponse, googleId);
+      var googleReq = https.request(getGooglePlusListOptions(googleId), function(googleListResponse) {
+        handleGooglesListResponse(res, googleListResponse, googleId);
       });
       googleReq.end();
     }
@@ -49,28 +49,40 @@ app.error(function(ex, req, res, next){
 //app.listen('/tmp/googleplusrss_node.socket');
 app.listen(11908);
 
-function getGoogleOptions(googleId) {
+function getGooglePlusListOptions(googleId) {
   return {
-          host: 'plus.google.com',
+          host: 'www.googleapis.com',
           port: 443,
-          path: '/_/stream/getactivities/?&sp=[1,2,"'+googleId+'",null,null,40,null,"social.google.com",[]]',
+          path: '/plus/v1/people/'+googleId+'/activities/public?key=AIzaSyBx1DExRwKKMsyjNyfO-5LVzBnaTSVSmp8',
           method: 'GET',
           headers: {
-            'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110613 Firefox/6.0a2',
             'Connection':'keep-alive'
           }
         };
 }
 
-function handleGooglesResponse(serverResponse, googleResponse, googleId) {
+function getGooglePlusGetOptions(activityId) {
+  return {
+          host: 'www.googleapis.com',
+          port: 443,
+          path: '/plus/v1/activities/'+activityId+'?key=AIzaSyBx1DExRwKKMsyjNyfO-5LVzBnaTSVSmp8',
+          method: 'GET',
+          headers: {
+            'Connection':'keep-alive'
+          }
+        };
+}
+
+function handleGooglesListResponse(serverResponse, googleListResponse, googleId) {
   try {
     var data = [];
-    googleResponse.on('data', function(d) {
+    googleListResponse.on('data', function(d) {
       data.push(d);
     });
-    googleResponse.on('end', function() {
-      if (googleResponse.statusCode != 200) { //output directions status code other then 200 came back from google
-        displayErrorPage("Google sent back a status code of: " + googleResponse.statusCode, serverResponse);
+    googleListResponse.on('end', function() {
+      //output directions status code other then 200 came back from google
+      if (googleListResponse.statusCode != 200) { 
+        displayErrorPage("Google sent back a status code of: " + googleListResponse.statusCode, serverResponse);
       } else {
         sendRSSfeed(serverResponse, data.join(''), googleId)
       }
@@ -80,145 +92,28 @@ function handleGooglesResponse(serverResponse, googleResponse, googleId) {
   }
 }
 
-// To clean up the results to parse the JSON object from google
-function googleJSONFilter(googleResults) {
-  googleResults = googleResults.replace(")]}'","");
-  googleResults = googleResults.replace(/\[,/g, '["",');
-  googleResults = googleResults.replace(/,,/g, ',"",');
-  return googleResults.replace(/,,/g, ',"",');
-}
-
 // This is the main function to turn googles JSON into a rss feed
-function sendRSSfeed(serverResponse, googleResults, googleId) {
+function sendRSSfeed(serverResponse, googleListResults, googleId) {
   try {
-    var p = JSON.parse(googleJSONFilter(googleResults));
-    var post = p[1][0];
-    var authorName = post[0][3].toString();
+    var googleFeed = JSON.parse(googleListResults);
+    
+    var authorName = googleFeed.items[0].actor.displayName;
+    console.log(googleFeed.items.length);
 
     var feed = new RSS({
-      title: authorName + "'s Google+ Public Feed",
+      title: googleFeed.title,
       feed_url: 'http://plus.google.com/' + googleId + '/posts',
       site_url: 'http://plus.google.com',
       author: authorName
     });
 
-    for (i=0;i<post.length;i++)
+    for (i=0;i<googleFeed.items.length;i++)
     {
-      var posttitle = ''; 
-      var postdescription = ''; 
-      var posturl = 'https://plus.google.com/' + post[i][21].toString(); // link to the item
-      var postdate = new Date(post[i][5]) // any format that js Date can parse.
-
-      var POST_TYPE = PostTypeEnum.Plain;
-      var POST_HTML = post[i][4].toString();
-      var POST_PLAIN_TEXT = post[i][20].toString();
-      var LINK_NAME = null;
-      var LINK_DESCRIPTION = null;
-      var LINK_URL = null;
-      var IMAGE_URL = null;
-      var USER_COMMENT_HTML = post[i][47].toString();
-      var USER_COMMENT_PLAIN_TEXT = post[i][48].toString();
-      var YOUTUBE_VIDEO_IMAGE_URL = null;
-      var SHARED_POST_USER_ID = null;
-      var SHARED_POST_NAME = null;
-
-      
-      if (post[i][11][0] != null) {
-        LINK_NAME = post[i][11][0][3].toString();
-        LINK_DESCRIPTION = post[i][11][0][21].toString();
-        POST_TYPE = PostTypeEnum.Link;
-        if (post[i][11][0][24] != null) {
-          LINK_URL = post[i][11][0][24][1].toString();
-          if (post[i][11][0][24][1].toString().indexOf("youtube.com") > 0) {
-            POST_TYPE = PostTypeEnum.YouTube;
-          }
-        }
-        if (post[i][11][0][41] != null && post[i][11][0][41][1] != null && post[i][11][0][41][1][1] != null) {
-          YOUTUBE_VIDEO_IMAGE_URL = post[i][11][0][41][1][1].toString();
-        }
-      }
-      if (post[i][11][1] != null && post[i][11][1][5] != null && post[i][11][1][5][1] != null) {
-        IMAGE_URL = post[i][11][1][5][1].toString();
-      } else {
-        if (post[i][11][0] != null && post[i][11][0][5] != null && post[i][11][0][5][1] != null) {
-          IMAGE_URL = post[i][11][0][5][1].toString();
-        }
-      }
-      if (post[i][43] != null && post[i][43][1] != null) {
-        POST_TYPE = PostTypeEnum.Shared;
-        SHARED_POST_USER_ID = post[i][43][1].toString();
-        SHARED_POST_NAME = post[i][43][0].toString();
-      }
-
-      switch(POST_TYPE) {
-      case PostTypeEnum.Link:
-        posttitle = POST_PLAIN_TEXT;
-        if (POST_HTML != null && POST_HTML != '') {
-          postdescription = postdescription + POST_HTML + "<br /><br />";
-        }
-        if (LINK_NAME != null && LINK_NAME != '' && LINK_URL != null && LINK_URL  != '') {
-          if (posttitle == null || posttitle == '') { // Set title to link name if no text was givin for the post.
-            posttitle = LINK_NAME;
-          }
-          postdescription = postdescription + "<a href='" + LINK_URL + "'>" + LINK_NAME + "</a><br />";
-        }
-        if (IMAGE_URL != null && IMAGE_URL != '') {
-          postdescription = postdescription + '<a href="' + posturl + '"><img src="' 
-                          + IMAGE_URL + '" border="0" /></a>';
-        }
-        postdescription = postdescription + post[i][11][0][21].toString();
-        break;
-      case PostTypeEnum.YouTube:
-        posttitle = POST_PLAIN_TEXT;
-        if (POST_HTML != null && POST_HTML != '') {
-          postdescription = postdescription + POST_HTML + "<br /><br />";
-        }
-        postdescription = postdescription + '<a href="' + posturl + '"><img src="' 
-                        + YOUTUBE_VIDEO_IMAGE_URL + '" border="0" title="Click to Play" /></a>';
-        postdescription = postdescription + "<br />";
-        postdescription = postdescription + LINK_DESCRIPTION;
-        break;
-      case PostTypeEnum.Shared:
-        if (USER_COMMENT_PLAIN_TEXT != null && USER_COMMENT_PLAIN_TEXT != '') {
-          posttitle = USER_COMMENT_PLAIN_TEXT;
-        } else {
-          posttitle = POST_PLAIN_TEXT;
-        }
-        if (USER_COMMENT_HTML != null && USER_COMMENT_HTML != '') {
-          postdescription = postdescription + USER_COMMENT_HTML + "<br /><br />";
-        }
-        postdescription = postdescription + '<a href="http://plus.google.com/' + SHARED_POST_USER_ID + '/posts">' 
-                                + SHARED_POST_NAME + '</a> shared this post: <br />';
-        if (POST_HTML != null && POST_HTML != '') {
-          postdescription = postdescription + POST_HTML + "<br /><br />";
-        }
-        if (LINK_URL != null && LINK_URL != '') {
-          postdescription = postdescription + "<a href='" + LINK_URL + "'>" + LINK_NAME + "</a><br />";
-          if (IMAGE_URL != null && IMAGE_URL != '') {
-            postdescription = postdescription + '<a href="' + posturl + '"><img src="' 
-                            + IMAGE_URL + '" border="0" /></a>';
-          }
-          postdescription = postdescription + LINK_DESCRIPTION;
-        }
-        break;
-      default:
-        posttitle = POST_PLAIN_TEXT;
-        postdescription = POST_HTML;
-        if (IMAGE_URL != null && IMAGE_URL != '') {
-          postdescription = postdescription + '<a href="' + posturl + '"><img src="' 
-                          + IMAGE_URL + '" border="0" /></a>';
-        }
-      }
-
-      if (posttitle.length > 100) {
-        posttitle = posttitle.substring(0, 97) + "..."
-      }
-
       feed.item({
-          title:  posttitle,
-          description: postdescription,
-          url: posturl,
-          date: postdate
+          title:  googleFeed.items[i].title,
+          description: '',
+          url: googleFeed.items[i].url,
+          date: googleFeed.items[i].updated
       });
     }
     var feed = feed.xml();
